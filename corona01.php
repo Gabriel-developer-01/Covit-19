568665bbf9b176c0f40ea23778aca635d4bee593
Gabriel-developer-01 <gabriel.mendes.souza1@hotmail.com>
2020-03-26 20:29:23 -0300

    Terminei o projeto,usei o Bootstrap para estilizar o site

--- corona01.php
+++ corona01.php
@@ -1,7 +1,5 @@
 <?php
 
-$targetCountry = $_GET['country'];
-
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -25,17 +23,44 @@ $Estatisticastotal = json_decode($ApiPrincipal, true);
 <head>
     <meta charset="UTF-8">
     <title>COVID-19</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">   
 </head>
-<body>
+<body style="background-color: cadetblue;">
 
 <div class="container">
 		<div class="jumbotron mt-3">
 		<h1 class="mb-2">COVID-19 Atualização de casos - By Gabriel</h1>
-        <form class="from-group" method="get">
-            <input class="form-control" type="text" name="country" placeholder="Verificar estatísticas pelo nome do seu país">
-		    <button class="btn btn-primary float-right mt-3" type="submit">Confira</button>
+        <form class="from-group" role="status" method="get">
+            <input class="form-control" type="text" name="country" placeholder="Verificar estatísticas atualizadas pelo nome do seu país"/>
+            <button class="btn btn-info float-right mt-3" type="submit">Confira</button>
+            
         </form>
-    
+
+    <?php if(!empty($data['confirmed'])): ?>
+        <table class="table table-hover">
+        <thead>
+            <tr>
+            <th scope="col"><?php echo htmlspecialchars($_GET['country'], ENT_QUOTES); ?></th>    
+            <th scope="col">Infectado</th>
+            <th scope="col">Recuperação</th>
+            <th scope="col">Mortes</th>
+            <th scope="col">Ùltima Atualização</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <td>#</td>
+            <td><?php echo number_format($data['confirmed']['value']) ?></td>
+            <td><?php echo number_format($data['recovered']['value']) ?></td>
+            <td><?php echo number_format($data['deaths']['value']) ?></td>
+            <td><?php echo $data['lastUpdate'] ?></td>
+            </tr>
+    <?php endif; ?>
+
+    <img class='mt-2 text-center' src='https://covid19.mathdro.id/api/og' height="300px" width="1050px" />
+	
+	</div>
+</div>
+
 </body>
 </html>
