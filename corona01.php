ac4768ad8675d75b7a7ab42408f5ba74c90a1b79
Gabriel-developer-01 <gabriel.mendes.souza1@hotmail.com>
2020-03-26 15:16:01 -0300

    após consumir os dados,estou criando o site

--- corona01.php
+++ corona01.php
@@ -3,20 +3,39 @@
 $targetCountry = $_GET['country'];
 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://covid19.mathdro.id/api/countries/' . $targetCountry);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-$result = curl_exec($ch);
+if(isset($_GET['country']) && !empty($_GET['country'])){
 
+curl_setopt($ch, CURLOPT_URL, 'https://covid19.mathdro.id/api/countries/' . urlencode($_GET['country']));
+$result = curl_exec($ch);
 $data = json_decode($result, true);
-//adicionando os dados//
- echo 'Covid 19 confirmado no ' . $targetCountry . ': ' . number_format($data['confirmed']['value']);
- echo "<br>";
- echo 'Covid 19 recuperação em ' . $targetCountry . ': ' . number_format($data['recovered']['value']);
- echo "<br>";
- echo 'Covid 19 mortos em ' . $targetCountry . ': ' .  number_format($data['deaths']['value']);
- echo "<br>";
- echo 'Ultima atualização: ' . $data['lastUpdate'];
-			
-?>
+
+curl_setopt($ch, CURLOPT_URL, 'https://covid19.mathdro.id/api');
+$ApiPrincipal = curl_exec($ch);
+$Estatisticastotal = json_decode($ApiPrincipal, true);
+
+}
+		
+?> 
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>COVID-19</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="container">
+		<div class="jumbotron mt-3">
+		<h1 class="mb-2">COVID-19 Atualização de casos - By Gabriel</h1>
+        <form class="from-group" method="get">
+            <input class="form-control" type="text" name="country" placeholder="Verificar estatísticas pelo nome do seu país">
+		    <button class="btn btn-primary float-right mt-3" type="submit">Confira</button>
+        </form>
+    
+</body>
+</html>
