c4ac66c6202ab38e3c57fef619d4376dd596058e
Gabriel-developer-01 <gabriel.mendes.souza1@hotmail.com>
2020-03-26 14:07:50 -0300

    adicionando os dados

--- corona01.php
+++ corona01.php
@@ -10,6 +10,13 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $result = curl_exec($ch);
 
 $data = json_decode($result, true);
-
-
+//adicionando os dados//
+ echo 'Covid 19 confirmado no ' . $targetCountry . ': ' . number_format($data['confirmed']['value']);
+ echo "<br>";
+ echo 'Covid 19 recuperação em ' . $targetCountry . ': ' . number_format($data['recovered']['value']);
+ echo "<br>";
+ echo 'Covid 19 mortos em ' . $targetCountry . ': ' .  number_format($data['deaths']['value']);
+ echo "<br>";
+ echo 'Ultima atualização: ' . $data['lastUpdate'];
+			
 ?>
